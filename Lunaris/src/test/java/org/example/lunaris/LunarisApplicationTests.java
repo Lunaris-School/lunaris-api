c12197680ab7a12962af4bc3379528eb8c06237c
clarabartolini <clarinhabartolini321@gmail.com>
2026-02-05 09:26:50 -0300

    feat: adicionar estrutura inicial do projeto com Maven e Spring Boot

---
+++ Lunaris/src/test/java/org/example/lunaris/LunarisApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.lunaris;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LunarisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
