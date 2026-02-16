62b93cfa39b09a0097e5f403d1f0567cd69ad71a
Giulia <giulia.gso.2008@gmail.com>
2026-02-16 00:22:39 -0300

    feat: implementando models e repositorys para as entidades

---
+++ Lunaris/src/main/java/org/example/lunaris/repository/AlunoRepository.java
@@ -0,0 +1,12 @@
+package org.example.lunaris.repository;
+
+import org.example.lunaris.model.Aluno;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface AlunoRepository extends JpaRepository<Aluno, Integer> {
+    Optional<Aluno> findByEmail(String email);
+}
